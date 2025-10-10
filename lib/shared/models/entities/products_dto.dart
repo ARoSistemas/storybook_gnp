d767162edd47ca8c97abf9696a96c9397c084fb1
Andy Rodriguez <ro.andres10@gmail.com>
2025-10-10 09:16:46 -0600

    Fix: Parseo de una lista en vex de un Json Object

--- lib/shared/models/entities/products_dto.dart
+++ lib/shared/models/entities/products_dto.dart
@@ -11,6 +11,31 @@ class Products {
     required this.rating,
     required this.isFavorite,
   });
+
+  // factory Products.fromRaw(String strRaw) =>
+  //     Products.fromMap(json.decode(strRaw));
+
+  factory Products.fromMap(Map<String, dynamic> json) => Products(
+    id: json['id'] ?? 0,
+    title: json['title'] ?? '',
+    price: (json['price'] as double?) ?? 0.0,
+    description: json['description'] ?? '',
+    category: json['category'] ?? '',
+    image: json['image'] ?? '',
+    rating: Rating.fromMap(json['rating']),
+    isFavorite: false,
+  );
+
+  factory Products.empty() => Products(
+    id: 0,
+    title: '',
+    price: 0,
+    description: '',
+    category: '',
+    image: '',
+    rating: Rating(rate: 0, count: 0),
+    isFavorite: false,
+  );
   int id;
   String title;
   double price;
@@ -20,21 +45,13 @@ class Products {
   Rating rating;
   bool isFavorite;
 
-  List<Products> fromRaw(String str) =>
-      List<Products>.from(json.decode(str).map((x) => Products.fromMap(x)));
+  // List<Products> fromRaw(String str) =>
+  //     List<Products>.from(jsonDecode(str).map((x) => Products.fromMap(x)));
 
-  // factory Products.fromRaw(String strRaw) =>
-  //     Products.fromMap(json.decode(strRaw));
-
-  factory Products.fromMap(Map<String, dynamic> json) => Products(
-    id: json["id"] ?? 0,
-    title: json["title"] ?? '',
-    price: json["price"]?.toDouble() ?? 0.0,
-    description: json["description"] ?? '',
-    category: json["category"] ?? '',
-    image: json["image"] ?? '',
-    rating: Rating.fromMap(json["rating"]),
-    isFavorite: false,
+  List<Products> fromRaw(String str) => List<Products>.from(
+    (jsonDecode(str) as List<Products>).map(
+      (x) => Products.fromMap(x as Map<String, dynamic>),
+    ),
   );
 
   Products copyWith({
@@ -56,28 +73,19 @@ class Products {
     rating: rating ?? this.rating,
     isFavorite: isFavorite ?? this.isFavorite,
   );
-
-  factory Products.empty() => Products(
-    id: 0,
-    title: '',
-    price: 0.0,
-    description: '',
-    category: '',
-    image: '',
-    rating: Rating(rate: 0.0, count: 0),
-    isFavorite: false,
-  );
 }
 
 class Rating {
   Rating({required this.rate, required this.count});
+
+  factory Rating.fromMap(Map<String, dynamic> json) => Rating(
+    rate: (json['rate'] as double?) ?? 0.0,
+    count: (json['count'] as int?) ?? 0,
+  );
 
   double rate;
   int count;
 
   Rating copyWith({double? rate, int? count}) =>
       Rating(rate: rate ?? this.rate, count: count ?? this.count);
-
-  factory Rating.fromMap(Map<String, dynamic> json) =>
-      Rating(rate: json["rate"]?.toDouble() ?? 0.0, count: json["count"] ?? 0);
 }
